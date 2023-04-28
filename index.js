2a089910c2a3cfa6a354d3ca6e836d1c9245dda3
Isabel <isabelkvaughan@Isabels-MacBook-Pro.local>
2023-04-28 17:11:56 +1000

    js to create svg added

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // import file system & inquirer
 const inquirer = require('inquirer');
 const fs = require('fs');
-const { Circle, Square, Triangle } = require('./shapes');
+const { Circle, Square, Triangle } = require('./lib/shapes.js');
+const { Text } = require('./lib/text.js');
 
 // questions 
 const questions = [
@@ -38,7 +39,37 @@ inquirer
   .prompt(questions)
   .then((answers) => {
     console.log('Answers:', answers);
-  })
-  .catch((error) => {
-    console.log('Error:', error);
-  });
+    
+// Create the SVG content based on the selected shape and text
+let svgContent;
+switch (answers.shape) {
+  case "Circle":
+    const circle = new Circle(answers.shapeColor);
+    svgContent = circle.render();
+    break;
+  case "Square":
+    const square = new Square(answers.shapeColor);
+    svgContent = square.render();
+    break;
+  case "Triangle":
+    const triangle = new Triangle(answers.shapeColor);
+    svgContent = triangle.render();
+    break;
+}
+
+const text = new Text(answers.text, answers.textColor);
+svgContent += text.render();
+
+// Create the SVG file
+const svgFileContent = `<svg xmlns="http://www.w3.org/2000/svg" width="300" height="200">${svgContent}</svg>`;
+fs.writeFile('logo.svg', svgFileContent, (err) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log('SVG file created: logo.svg');
+  }
+});
+})
+.catch((error) => {
+console.log('Error:', error);
+});
